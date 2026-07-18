351bbe583d2962dabaa7193cd868d61cdadac624
Rohan Shetty <rshe0040@student.monash.edu>
2026-07-18 22:15:01 +1000

    Test works

--- Assets/Scripts/DebugTrigger.cs
+++ Assets/Scripts/DebugTrigger.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DebugTrigger : MonoBehaviour
 {
@@ -7,6 +8,11 @@ public class DebugTrigger : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.T))
         {
             MemoryManager.Instance.Unlock(ref MemoryManager.Instance.touchUnlocked);
+        }
+
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            SceneManager.LoadScene("TestRoom2");
         }
     }
 }
