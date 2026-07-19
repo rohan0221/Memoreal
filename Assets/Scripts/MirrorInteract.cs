93df36e19856a7da0a8988e878379374fe79b65e
Rohan Shetty <rshe0040@student.monash.edu>
2026-07-19 16:11:00 +1000

    Add MirrorInteract script

---
+++ Assets/Scripts/MirrorInteract.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class MirrorInteract : MonoBehaviour
+{
+    public GameObject mirrorCameraRig;
+    public Camera mainCamera;
+    public MonoBehaviour playerMovementScript; // whatever your player controller script is called
+    bool playerNearby;
+    bool inMirrorView;
+
+    void Update()
+    {
+        if (playerNearby && !inMirrorView && Input.GetKeyDown(KeyCode.E))
+        {
+            EnterMirror();
+        }
+        else if (inMirrorView && (Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.Escape)))
+        {
+            ExitMirror();
+        }
+    }
+
+    void EnterMirror()
+    {
+        inMirrorView = true;
+        mainCamera.gameObject.SetActive(false);
+        mirrorCameraRig.SetActive(true);
+        if (playerMovementScript != null) playerMovementScript.enabled = false;
+    }
+
+    void ExitMirror()
+    {
+        inMirrorView = false;
+        mirrorCameraRig.SetActive(false);
+        mainCamera.gameObject.SetActive(true);
+        if (playerMovementScript != null) playerMovementScript.enabled = true;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) playerNearby = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player")) playerNearby = false;
+    }
+}
