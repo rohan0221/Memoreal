2957a6dcb0f95bc311295fd246758cad95ecfbbf
Rohan Shetty <rshe0040@student.monash.edu>
2026-07-19 16:28:05 +1000

    Edit debug

--- Assets/Scripts/DebugTrigger.cs
+++ Assets/Scripts/DebugTrigger.cs
@@ -6,13 +6,18 @@ public class DebugTrigger : MonoBehaviour
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.T))
-        {
             MemoryManager.Instance.Unlock(ref MemoryManager.Instance.touchUnlocked);
-        }
+
+        if (Input.GetKeyDown(KeyCode.Y))
+            MemoryManager.Instance.Unlock(ref MemoryManager.Instance.tasteUnlocked);
+
+        if (Input.GetKeyDown(KeyCode.U))
+            MemoryManager.Instance.Unlock(ref MemoryManager.Instance.hearingUnlocked);
+
+        if (Input.GetKeyDown(KeyCode.I))
+            MemoryManager.Instance.Unlock(ref MemoryManager.Instance.smellUnlocked);
 
         if (Input.GetKeyDown(KeyCode.N))
-        {
             SceneManager.LoadScene("TestRoom2");
-        }
     }
 }
