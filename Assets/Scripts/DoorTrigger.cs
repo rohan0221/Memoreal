bc94e0afe1870b6cf2daefa712c589044055a704
Rohan Shetty <rshe0040@student.monash.edu>
2026-07-25 14:36:54 +1000

    Add door trigger transition

---
+++ Assets/Scripts/DoorTrigger.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+    public string targetSceneName;
+    public string targetSpawnPointName;
+    bool playerNearby;
+
+    void Update()
+    {
+        if (playerNearby && Input.GetKeyDown(KeyCode.E))
+        {
+            InteractPromptUI.Instance.Hide();
+            SceneTransitionManager.Instance.TransitionTo(targetSceneName, targetSpawnPointName);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerNearby = true;
+            InteractPromptUI.Instance.Show(transform);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerNearby = false;
+            InteractPromptUI.Instance.Hide();
+        }
+    }
+}
