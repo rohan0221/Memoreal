fe9bf55a2333f780f1f7061385b60a3c51be848b
Rohan Shetty <rshe0040@student.monash.edu>
2026-07-26 18:52:06 +1000

    Add all mirror drwaings

--- Assets/Scripts/MirrorViewController.cs
+++ Assets/Scripts/MirrorViewController.cs
@@ -4,14 +4,22 @@ using TMPro;
 
 public class MirrorViewController : MonoBehaviour
 {
-    public GameObject viewCanvas; // parent object containing the image, toggled on/off
+    public GameObject viewCanvas;
     public RectTransform imageRect;
     public Image displayImage;
-    public Sprite[] frames; // 2 frames to alternate between
+
+    public Sprite[] eyesOnlyFrames;  // default, nothing unlocked yet
+    public Sprite[] touchFrames;     // hands revealed
+    public Sprite[] hearingFrames;   // ears revealed
+    public Sprite[] smellFrames;     // nose revealed
+    public Sprite[] tasteFrames;     // mouth revealed
+
     public float jitterInterval = 0.15f;
     public float maxParallaxOffset = 40f;
-    public GameObject captionRoot; // small text object inside MirrorViewCanvas, start disabled
+    public GameObject captionRoot;
     public TextMeshProUGUI captionText;
+
+    Sprite[] activeFrames;
     Vector2 basePosition;
     Vector2 accumulatedOffset;
     float jitterTimer;
@@ -24,14 +32,25 @@ public class MirrorViewController : MonoBehaviour
         viewCanvas.SetActive(false);
     }
 
+    Sprite[] GetCurrentStageFrames()
+    {
+        var m = MemoryManager.Instance;
+        if (m.tasteUnlocked) return tasteFrames;
+        if (m.smellUnlocked) return smellFrames;
+        if (m.hearingUnlocked) return hearingFrames;
+        if (m.touchUnlocked) return touchFrames;
+        return eyesOnlyFrames;
+    }
+
     public void StartViewing()
     {
         isViewing = true;
         viewCanvas.SetActive(true);
+        activeFrames = GetCurrentStageFrames();
         frameIndex = 0;
         jitterTimer = 0f;
         accumulatedOffset = Vector2.zero;
-        displayImage.sprite = frames[0];
+        displayImage.sprite = activeFrames[0];
     }
 
     public bool IsViewing => isViewing;
@@ -44,13 +63,13 @@ public class MirrorViewController : MonoBehaviour
         if (jitterTimer >= jitterInterval)
         {
             jitterTimer = 0f;
-            frameIndex = (frameIndex + 1) % frames.Length;
-            displayImage.sprite = frames[frameIndex];
+            frameIndex = (frameIndex + 1) % activeFrames.Length;
+            displayImage.sprite = activeFrames[frameIndex];
         }
 
         float mouseX = Input.GetAxis("Mouse X");
         float mouseY = Input.GetAxis("Mouse Y");
-        accumulatedOffset += new Vector2(-mouseX, -mouseY) * 1f;
+        accumulatedOffset += new Vector2(-mouseX, -mouseY) * 10f;
         accumulatedOffset = Vector2.ClampMagnitude(accumulatedOffset, maxParallaxOffset);
 
         imageRect.anchoredPosition = basePosition + accumulatedOffset;
