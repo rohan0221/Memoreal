f74c27e84ec58a3199f77a995df88bdc62c611bf
Rohan Shetty <rshe0040@student.monash.edu>
2026-07-31 23:11:15 +1000

    Add real images

--- Assets/Scripts/CinematicIntroController.cs
+++ Assets/Scripts/CinematicIntroController.cs
@@ -5,24 +5,28 @@ using UnityEngine.UI;
 public class CinematicIntroController : MonoBehaviour
 {
     public Image contentImage;
-    public Sprite blurryImage;
-    public Sprite clearImage;
+    public Sprite[] blurryFrames;
+    public Sprite[] clearFrames;
+    public float jitterInterval = 0.15f;
     public CinematicParallax parallaxScript;
     public float gateDuration = 4f;
     public string nextSceneName = "HospitalRoom";
     public string nextSpawnPointName = "BedSpawnPoint";
+
+    Coroutine jitterRoutine;
 
     IEnumerator Start()
     {
         var vignette = SceneTransitionManager.Instance.eyeVignette;
         parallaxScript.enabled = false;
 
-        contentImage.sprite = blurryImage;
+        jitterRoutine = StartCoroutine(JitterLoop(blurryFrames));
         yield return StartCoroutine(vignette.PlayOpen());
         yield return new WaitForSeconds(0.3f);
         yield return StartCoroutine(vignette.PlayClose());
+        StopCoroutine(jitterRoutine);
 
-        contentImage.sprite = clearImage;
+        jitterRoutine = StartCoroutine(JitterLoop(clearFrames));
         yield return StartCoroutine(vignette.PlayOpen());
 
         parallaxScript.enabled = true;
@@ -30,7 +34,19 @@ public class CinematicIntroController : MonoBehaviour
         yield return new WaitForSeconds(gateDuration);
         yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.Space));
 
+        StopCoroutine(jitterRoutine);
         parallaxScript.enabled = false;
         SceneTransitionManager.Instance.TransitionTo(nextSceneName, nextSpawnPointName);
+    }
+
+    IEnumerator JitterLoop(Sprite[] frames)
+    {
+        int index = 0;
+        while (true)
+        {
+            contentImage.sprite = frames[index % frames.Length];
+            index++;
+            yield return new WaitForSeconds(jitterInterval);
+        }
     }
 }
